01a45a690ff2d460f8306d2ccde75f088ff4a1e2
tnsihy <tnsihy@foxmail.com>
2019-04-27 19:45:47 +0800

    1.1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+</style>
