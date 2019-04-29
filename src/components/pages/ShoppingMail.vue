a7228b420f10b30b949bcef7486aaa86e307b74f
tnsihy <tnsihy@foxmail.com>
2019-04-29 23:21:07 +0800

    2019/4/29

--- src/components/pages/ShoppingMail.vue
+++ src/components/pages/ShoppingMail.vue
@@ -13,17 +13,53 @@
         </van-col>
       </van-row>
     </div>
+    <!-- 2.swiper 轮播图 :autoplay:1000 轮播时间1秒-->
+    <div class="swiper-area">
+      <van-swipe :autoplay="2000">
+        <van-swipe-item v-for="(banner,index) in bannerPicArray" :key="index">
+          <!-- 3.<img :src="banner.imageUrl" width="100%"> 改为懒加载v-lazy-->
+          <img v-lazy="banner.image" width="100%">
+        </van-swipe-item>
+      </van-swipe>
+    </div>
+    <!--6.type bar -->
+    <div class="type-bar">
+      <div v-for="(cate,index) in category" :key="index">
+        <img v-lazy="cate.image" width="90%">
+        <span>{{cate.mallCategoryName}}</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
       msg: "Shopping Mail",
-    //   ../../通过两级找到assets
-      locationIcon:require('../../assets/images/location.png')
-    };
+    // 1.定位  ../../通过两级找到assets
+      locationIcon:require('../../assets/images/location.png'),
+      bannerPicArray:[],
+      category:[]
+    }
+  },
+  created(){
+    axios({
+      url:'https://www.easy-mock.com/mock/5cc6f48758e3d93eff3d80a7/SmileDemo/index',
+      method:'get'
+    })
+    .then(response=>{
+      console.log(response) //4.可以打印观察测试数据
+      if(response.status === 200){
+        //5.如果请求成功
+        this.category = response.data.data.category;
+        this.bannerPicArray = response.data.data.slides;
+      }
+    })
+    .catch(error=>{
+
+    })
   }
 };
 </script>
@@ -33,6 +69,7 @@ export default {
     height:2.2rem;
     line-height: 2.2rem;
     background-color:rgb(72, 226, 213);
+    overflow: hidden;
 }
 .location-icon{
     width:80%;
@@ -43,11 +80,32 @@ export default {
     height: 1.3rem;
     border:none;
     border-bottom:1px solid #fff !important;
-    background-color: rgb(72, 226, 213);
+    background-color:rgb(72, 226, 213);
     color:#fff;
 }
 .search-button{
     border-radius: 0.31rem;
-    margin-left:.2rem;
+    background-color:fff;
+    margin-left:.5rem;
+}
+.swiper-area{
+  clear: both;/* 清除浮动 */
+  max-height:13rem;/* 懒加载的bug，限制轮播点的最大高度 */
+  overflow: hidden;
+}
+.type-bar{
+  background: #fff;
+  margin:0 .3rem .3rem .3rem;
+  border-radius: .3rem;
+  font-size: 14px;
+  display: flex;
+  flex-direction: row;/* (默认)横向排列 */
+  flex-wrap: nowrap;/* (默认)不换行 */
+}
+.type-bar div{
+  padding:.3rem;
+  font-size:12px;
+  text-align: center;
+  max-width: 60px; /* 图片大小可能不同，调整图片大小 */
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {Button,Row,Col} from 'vant'
+// 使用时引入vant中组件
+import {Button,Row,Col,Swipe,SwipeItem,Lazyload} from 'vant'
 
-Vue.use(Button).use(Row).use(Col)
+Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload)
 Vue.config.productionTip = false
 
 new Vue({
